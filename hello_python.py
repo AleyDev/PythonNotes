328acf9421ee3538f901136f1218f1b27dd2dfdb
Aleyna Kaya <devaleykaya@gmail.com>
2024-06-22 23:38:30 +0300

    print fonksiyonu

---
+++ hello_python.py
@@ -0,0 +1,9 @@
+"""
+“print()” fonksiyonu, kendisine verilen herhangi bir değeri ekrana yazdırır.
+
+Ekrana “Hello Python!” yazdırmak için kullanılan temel bir Python komutudur. 
+
+"""
+
+
+print("Hello Python!") 
